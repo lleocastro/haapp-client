a1ca9439775bbc1cd80b04984b292fb89d6ca908
Léo B. Castro <leonardo_carvalho@outlook.com>
2018-04-29 23:01:07 +0000

    WIP

--- src/pages/secrets/secrets.ts
+++ src/pages/secrets/secrets.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController, ActionSheetController } from 'ionic-angular';
 
 import { UserProvider } from '../../providers/auth/user';
 import { SecretsProvider } from '../../providers/secrets/secrets';
@@ -19,7 +19,8 @@ export class SecretsPage {
   constructor(
     private navCtrl: NavController,
     private secretsProvider: SecretsProvider,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private actionSheetCtrl: ActionSheetController
   ) {
     this.secrets = [];
     this.currentUser = UserProvider.getUser();
@@ -32,6 +33,33 @@ export class SecretsPage {
 
   showSecretImage(e, secret) {
     secret.showImage = true;
+  }
+
+  openSecretOptions(e, secret) {
+    secret.openedOptions = true;
+
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Denunciar',
+          icon: 'ios-warning-outline',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: 'Cancelar',
+          icon: 'ios-close-circle-outline',
+          role: 'cancel',
+          handler: () => {
+            secret.openedOptions = false;
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
   }
 
   goToQuote(e) {
